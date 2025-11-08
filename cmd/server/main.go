c86b0f08fe2d845dbeb6adb45a0a84ef47cc0666
Sérgio Costa <sergio.md.costa@protonmail.com>
2025-11-08 21:33:26 +0000

    feat(temp): Return temperatures

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"net/http"
+	"fmt"
 
+	"github.com/sergioc0sta/go-cloud-run/configs"
 	"github.com/sergioc0sta/go-cloud-run/internal/infra/handlers"
+	"github.com/sergioc0sta/go-cloud-run/internal/infra/temperature"
 )
 
 func main() {
+	cfg, _ := configs.LoadConfig(".")
+
+	viaCepAPI := cfg.ViaCepAPI
+	tempAPI := cfg.WeatherAPI
+
 	mux := http.NewServeMux()
+	temperatureClient := temperature.NewTemperatureClient(viaCepAPI, tempAPI) 
 
 	mux.HandleFunc("/health", handlers.HealthHandler)
-	mux.HandleFunc("/a", handlers.WeatherHandler)
+	mux.HandleFunc("/temp", handlers.WeatherHandler(temperatureClient))
 
 	http.ListenAndServe(":8081", mux)
+	fmt.Println("Server running on port 8081")
 }
--- internal/infra/handlers/weather.go
+++ internal/infra/handlers/weather.go
@@ -1,64 +1,39 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"strconv"
 
-	"github.com/sergioc0sta/go-cloud-run/configs"
+	"github.com/sergioc0sta/go-cloud-run/internal/infra/temperature"
 	"github.com/sergioc0sta/go-cloud-run/internal/validate"
 )
 
-type WeatherResponse struct {
-	Location string `json:"localidade"`
+type FetchTemp interface {
+	FetchTemperature(ctx context.Context, cep string) (*temperature.TemperatureResponse, error)
 }
 
-func fetchWeatherByCEP(cep string) (*WeatherResponse, error) {
-	url := "http://viacep.com.br/ws/" + cep + "/json/"
+func WeatherHandler(fetchTemp FetchTemp) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
+		cep := r.URL.Query().Get("cep")
+		idValid := validate.CepValidator(cep)
+
+		if !idValid {
+			http.Error(w, "invalid zipcode", http.StatusUnprocessableEntity)
+			return
+		}
+
+		temperature, err := fetchTemp.FetchTemperature(r.Context(), cep)
+		if err != nil || temperature == nil {
+			http.Error(w, "can not find zipcode", http.StatusNotFound)
+			return
+		}
+
+		respBytes, _ := json.Marshal(temperature)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(respBytes)
 	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("external service status: %d", resp.StatusCode)
-	}
-
-	var data WeatherResponse
-	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		return nil, err
-	}
-	return &data, nil
-}
-
-func WeatherHandler(w http.ResponseWriter, r *http.Request) {
-	configs, err := configs.LoadConfig(".")
-
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Configs: %+v\n", configs)
-
-	cep := r.URL.Query().Get("cep")
-	idValid := validate.CepValidator(cep)
-
-	if !idValid {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		w.Write([]byte("invalid zipcode"))
-		return
-	}
-
-	data, err := fetchWeatherByCEP(cep)
-	if err != nil {
-		http.Error(w, "failed to call external service", http.StatusBadGateway)
-		return
-	}
-
-	fmt.Printf("CEP data: %+v\n", data)
-
-	w.Write([]byte("Weather data for CEP: " + strconv.FormatBool(idValid)))
 }
